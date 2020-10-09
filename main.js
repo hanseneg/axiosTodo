69f8e5989e85e7c57297ab04726fafd250851433
Ethan Hansen <hanseneg@hotmail.com>
2020-10-09 14:37:30 -0600

    commit3

---
+++ main.js
@@ -0,0 +1,10 @@
+const title = document.getElementById('title')
+const description = document.getElementById('description')
+const image = document.getElementById('image')
+const price = document.getElementById('price')
+const submit = document.getElementById('submit')
+const delet = document.getElementById('delet')
+const list = document.getElementById('list')
+
+
+axios.get("https://api.vschool.io/ethan17/todo")
