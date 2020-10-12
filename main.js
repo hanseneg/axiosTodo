ad5d72d5a6569ecf10e7c458b64abcd4de410001
Ethan Hansen <hanseneg@hotmail.com>
2020-10-12 15:41:07 -0600

    commit5

--- main.js
+++ main.js
@@ -25,9 +25,111 @@ axios.get("https://api.vschool.io/ethan17/todo")
             div.appendChild(label)
             list.appendChild(div)
         }
+        if(response.data[i].completed === true){
+            label.strike()
+        }
     })
     .catch(error => console.log(error))
 
 //5f7cb303a269067180b0f22b
 
+
+//put
 axios.put("https://api.vschool.io/ethan17/todo/5f7cb303a269067180b0f22b", updates)
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error))
+
+// const updates = {
+//     title: "My Second Todo",
+//     description: "This is my second todo"
+// }
+
+// axios.put("https://api.vschool.io/scrimbalessons/todo/5d8bd531ee91575e6d49e06f", updates)
+//     .then(response => console.log(response.data))
+//     .catch(error => console.log(error))
+
+//post
+const todoForm = document.todoform
+
+todoForm.addEventListener("submit", function(event){
+    event.preventDefault()
+    
+    const newTodo = {
+        title: todoForm.title.value,
+        description: todoForm.description.value,
+        imgUrl: todoForm.imgUrl.value
+    }
+    
+    axios.post("https://api.vschool.io/ethan17/todo", newTodo)
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+    
+})
+
+//delete
+const button = document.getElementById('delete-button')
+
+button.addEventListener("click", function(){
+    axios.delete("https://api.vschool.io/scrimbalessons/todo/5d8bd511ee91575e6d49e06e")
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+})
+
+
+
+
+
+
+
+
+//auto update
+
+
+// GET's THE TODO's FROM THE DATABASE
+// function getData(){
+//     axios.get("https://api.vschool.io/johnsmith2/todo")
+//         .then(res => listData(res.data))
+//         .catch(err => console.log(err))
+// }
+
+
+
+// // LISTS THE TODO TITLES TO THE DOM
+// function listData(data){
+//     // document.getElementById('todo-list').innerHTML = ""
+//     clearList()
+    
+//     for(let i = 0; i < data.length; i++){
+//         const h1 = document.createElement('h1')
+//         h1.textContent = data[i].title
+//         document.getElementById('todo-list').appendChild(h1)
+//     }
+// }
+
+// function clearList(){
+//     const el = document.getElementById('todo-list')
+//     while(el.firstChild){
+//         el.removeChild(el.firstChild)
+//     }
+// }
+
+// getData()
+
+
+// // FORM FOR THE POST REQUEST
+// const todoForm = document["todo-form"]
+
+// todoForm.addEventListener("submit", function(e){
+//     e.preventDefault()
+    
+//     const newTodo = {
+//         title: todoForm.title.value
+//     }
+    
+//     todoForm.title.value = ""
+    
+//     axios.post("https://api.vschool.io/johnsmith2/todo", newTodo)
+//         .then(res => getData())
+//         .catch(err => console.log(err))
+// })
+
