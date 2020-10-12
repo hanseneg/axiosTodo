31fd112c5f896ecdf5ca7f682a13b23ceb71fc66
Ethan Hansen <hanseneg@hotmail.com>
2020-10-12 10:22:33 -0600

    commit4

--- main.js
+++ main.js
@@ -3,8 +3,31 @@ const description = document.getElementById('description')
 const image = document.getElementById('image')
 const price = document.getElementById('price')
 const submit = document.getElementById('submit')
-const delet = document.getElementById('delet')
+const deleteForm = document.getElementById('deleteForm')
+const deleteButton = document.getElementById('deleteButton')
 const list = document.getElementById('list')
 
+const updates = {
+    completed: true
+}
 
 axios.get("https://api.vschool.io/ethan17/todo")
+    .then(response => {
+        for(let i = 0; i < response.data.length; i++){
+            const check = document.createElement('input')
+            const div = document.createElement('div')
+            const label = document.createElement('label')
+            check.type = 'checkbox'
+            check.id = 'text'
+            label.for = 'text'
+            label.innerText = response.data[i].title
+            div.appendChild(check)
+            div.appendChild(label)
+            list.appendChild(div)
+        }
+    })
+    .catch(error => console.log(error))
+
+//5f7cb303a269067180b0f22b
+
+axios.put("https://api.vschool.io/ethan17/todo/5f7cb303a269067180b0f22b", updates)
